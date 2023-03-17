36c6b6e7439020cd1c755e20fbc6b6c1f5d6d41f
JungYeon <72572045+Jungyeon-Choi-hubbb@users.noreply.github.com>
2023-03-17 13:31:29 +0900

    ch4.03

---
+++ src/main/java/com/fastcampus/ch4/domain/PageHandler.java
@@ -0,0 +1,128 @@
+package com.fastcampus.ch4.domain;
+
+public class PageHandler {
+    private int totalCnt; // 총 게시물 갯수
+    private int pageSize; // 한 페이지의 크기
+    private int naviSize = 10; // 페이지 내비게이션의 크기
+    private int totalPage; // 전체 페이지의 갯수
+    private int page; // 현재 페이지
+    private int beginPage; // 내비게이션의 첫번째 페이지
+    private int endPage; // 내비게이션의 마지막 페이지
+    private boolean showPrev; // 이전 페이지로 이동하는 링크를 보여줄 것인지의 여부
+    private boolean showNext; // 다음 페이지로 이동하는 링크를 보여줄 것인지의 여부
+
+    public PageHandler(int totalCnt, int page) {
+        this(totalCnt, page, 10);
+    }
+
+    public PageHandler(int totalCnt, int page, int pageSize) {
+        this.totalCnt = totalCnt;
+        this.page = page;
+        this.pageSize = pageSize;
+
+        totalPage = (int)Math.ceil(totalCnt / (double)pageSize); // 남으면 페이지가 하나 더 필요해서 올림
+        beginPage = page / naviSize * naviSize + 1;
+        endPage = Math.min(beginPage + naviSize -1 , totalPage); // totalPage보다 작을 수 있으니 Math.min 사용해서 둘 중 작은 수 사용
+        showPrev = beginPage != 1;
+        showNext = endPage != totalPage;
+    }
+
+    public int getTotalCnt() {
+        return totalCnt;
+    }
+
+    public void setTotalCnt(int totalCnt) {
+        this.totalCnt = totalCnt;
+    }
+
+    public int getPageSize() {
+        return pageSize;
+    }
+
+    public void setPageSize(int pageSize) {
+        this.pageSize = pageSize;
+    }
+
+    public int getNaviSize() {
+        return naviSize;
+    }
+
+    public void setNaviSize(int naviSize) {
+        this.naviSize = naviSize;
+    }
+
+    public int getTotalPage() {
+        return totalPage;
+    }
+
+    public void setTotalPage(int totalPage) {
+        this.totalPage = totalPage;
+    }
+
+    public int getPage() {
+        return page;
+    }
+
+    public void setPage(int page) {
+        this.page = page;
+    }
+
+    public int getBeginPage() {
+        return beginPage;
+    }
+
+    public void setBeginPage(int beginPage) {
+        this.beginPage = beginPage;
+    }
+
+    public int getEndPage() {
+        return endPage;
+    }
+
+    public void setEndPage(int endPage) {
+        this.endPage = endPage;
+    }
+
+    public boolean isShowPrev() {
+        return showPrev;
+    }
+
+    public void setShowPrev(boolean showPrev) {
+        this.showPrev = showPrev;
+    }
+
+    public boolean isShowNext() {
+        return showNext;
+    }
+
+    public void setShowNext(boolean showNext) {
+        this.showNext = showNext;
+    }
+
+    // 페이지 내비게이션을 print하는 메서드
+    void print() {
+        System.out.println("page = " + page);
+        System.out.print(showPrev ? "[PREV] " : ""); // 참이면 이전페이지 가는 링크
+
+        for(int i = beginPage; i <= endPage; i++) {
+            System.out.print(i + " ");
+        }
+        System.out.println(showPrev ? "[Next] " : "");
+    }
+
+    @Override
+    public String toString() {
+        return "PageHandler{" +
+                "totalCnt=" + totalCnt +
+                ", pageSize=" + pageSize +
+                ", naviSize=" + naviSize +
+                ", totalPage=" + totalPage +
+                ", page=" + page +
+                ", beginPage=" + beginPage +
+                ", endPage=" + endPage +
+                ", showPrev=" + showPrev +
+                ", showNext=" + showNext +
+                '}';
+    }
+}
+
